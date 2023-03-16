87cb1f3ce6c13f3fa009030bebce5d2cdf45963e
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-03-16 14:50:41 -0500

    Function that produces a side effect

---
+++ lighthouse-js-fundamentals/hello.js
@@ -0,0 +1,19 @@
+//        Declaring and Calling Functions
+
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+// Two main varieties of functions:
+// One that produces a side effect, as in it does something. Example:
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
