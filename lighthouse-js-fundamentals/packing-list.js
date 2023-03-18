e902edcb1a5878707f2c581fd009d767a5a7fce3
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-03-18 14:27:18 -0500

    Iterating over an Array with while loop

--- lighthouse-js-fundamentals/packing-list.js
+++ lighthouse-js-fundamentals/packing-list.js
@@ -11,3 +11,16 @@ for (let i = 0; i < packingList.length; i++) {
 
 // If we run this code, we can see that the program reminds us to pack all the items in our kitchen. Looking more deeply into the code, this works because we're declaring a counter variable called i (short for "index") that starts at 0, and increments by 1 at every step of a loop until it matches the length of our array, that is packingList. You might remember that to access an item in an array we index it.
 
+// Iterating Over an Array With while
+
+
+// So for loops are a pretty good option when we want to iterate over items in an array. But that doesn't mean we can't use a while loop to do it.
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+// The result here will be exactly the same as before, but we've had to separate out the logic onto a few more lines.
